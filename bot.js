f9e65dae6f80fd4b44062849b16c9b8a87c5e400
ryu <ryooooooo1@gmail.com>
2018-12-08 03:29:52 +0900

    リファクタリング

--- bot.js
+++ bot.js
@@ -1,49 +1,28 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
-
-const channelId = process.env.CHANNEL_ID
 const userTable = {}
-const numOfMember = 5
-for (let i = 0; i < numOfMember; i++) {
-  userTable[process.env[`USER_ID_${i}`]] = { in: 0, out: 0 }
-}
 
-console.log(userTable)
 bot.on('ready', () => {
   console.log("Hello! I'm Reco.")
-})
+  const channel = bot.channels.find(val => val.name === 'reco')
 
-bot.on('message', message => {
-  if (message.content === 'bit') {
-    message.reply('bot')
-  } else if (message.content === 'bot') {
-    message.reply('bit')
-  }
-})
-
-bot.on('voiceStateUpdate', (oldMan, newMan) => {
-  if (newMan.voiceChannel) {
-    console.log(
-      `${newMan.displayName}が${newMan.voiceChannel.name}に入室しました`,
-      newMan.user.id
-    )
-    userTable[`${newMan.user.id}`].in = new Date()
-  }
-  if (oldMan.voiceChannel) {
-    console.log(
-      `${oldMan.displayName}が${oldMan.voiceChannel.name}を退室しました`,
-      oldMan.user.id
-    )
-    if (!newMan.voiceChannel) {
+  bot.on('voiceStateUpdate', (oldMan, newMan) => {
+    if (newMan.voiceChannel) {
+      const userId = newMan.user.id
+      if (!userTable[userId]) {
+        userTable[userId] = { in: 0, out: 0 }
+      }
+      userTable[`${userId}`].in = new Date()
+    }
+    if (oldMan.voiceChannel && !newMan.voiceChannel) {
       const user = userTable[`${newMan.user.id}`]
       user.out = new Date()
       const workingTime = Math.floor((user.out - user.in) / 60000)
-      const text = `${oldMan.displayName}さんは${workingTime}分働きました`
-      bot.channels
-        .find(val => val.id === channelId)
-        .send(`${text}`, { reply: newMan.user })
+      const text = `${workingTime}分間お勤めお疲れ様です！`
+      channel.send(`${text}`, { reply: newMan.user })
     }
-  }
+  })
 })
+
 const token = process.env.TOKEN
 bot.login(token)
